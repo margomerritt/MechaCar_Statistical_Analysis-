97a7779a4a75aec63fb3513df106692ee8af2c0c
margomerritt <missmargomerritt@gmail.com>
2023-01-25 17:24:25 -0500

    Updating R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,9 +6,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg) #create linear model
 
-summary(lm(mpg~vehicle_length,mechaCar_mpg)) #summarize linear model
+#summary(lm(mpg~vehicle_length,mechaCar_mpg)) #summarize linear model
 
+#summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mechaCar_mpg)) #summarize linear model
 
+#summary(lm(mpg~vehicle_weight,mechaCar_mpg)) #summarize linear model
 
+#summary(lm(mpg~spoiler_angle,mechaCar_mpg)) #summarize linear model
 
+#summary(mechaCar_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_mpg)) #generate summary statistics
 
