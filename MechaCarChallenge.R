8a2cf022a42640783ae2586c78e42f3059252f98
margomerritt <missmargomerritt@gmail.com>
2023-01-25 15:20:56 -0500

    Updating R files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,14 @@
 library(dplyr)
 
-mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg) #create linear model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCar_mpg) #create linear model
 
-summary(lm(mpg~vehicle_length,mechacar_mpg)) #summarize linear model
+#lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg) #create linear model
+
+summary(lm(mpg~vehicle_length,mechaCar_mpg)) #summarize linear model
+
+
 
 
 
