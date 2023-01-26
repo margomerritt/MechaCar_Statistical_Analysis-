03e12a80a55035e74287d5367a379d1b81efcec7
margomerritt <missmargomerritt@gmail.com>
2023-01-26 15:51:24 -0500

    Updating code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,25 @@
 library(dplyr)
+library(tidyverse)
+library(magrittr)
 
-mechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+# Deliverable 1 code: Linear Regression to Predict MPG
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCar_mpg) #create linear model
+#mechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) # Read in csv file
 
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_mpg)) #generate summary statistics
+#lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCar_mpg) #create linear model
+
+#summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_mpg)) #generate summary statistics
+
+
+# Deliverable 2 code: Create Visualizations for the Trip Analysis
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) # Read in csv file
+
+summarize_suspension <- suspension_coil %>% summarize(Mean=mean(PSI),
+                                                      Median=median(PSI),
+                                                      Variance=var(PSI),
+                                                      SD=sd(PSI),
+                                                      .groups = 'keep')
+
+
 
